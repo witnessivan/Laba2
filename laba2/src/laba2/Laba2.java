72d84e3baf07a358019928622021628fe0a2759c
Witnessvan <32366597+witnessivan@users.noreply.github.com>
2018-10-16 05:53:52 +0300

    Add files via upload

--- laba2/src/laba2/Laba2.java
+++ laba2/src/laba2/Laba2.java
@@ -18,6 +18,7 @@ public class Laba2 {
                 case 0:
                     text = input_text(text);
                     schet_texta(text);
+                    given_letter(text);
                     break;
                 case 1:
                     break;
@@ -39,5 +40,28 @@ public class Laba2 {
        char []array_char = text2.toCharArray();
        System.out.println(array_char.length);
        System.out.println(Arrays.toString(array_char));
+    }
+    static void number_reduction()
+    {}
+    static void given_letter(String text)
+    {
+        int temp = 0;
+        char []array_char = text.toCharArray();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Задайте букву");
+        String simvol = input.nextLine();
+        char []simvol_array = simvol.toCharArray();
+        if(array_char[0] == simvol_array[0])
+        {
+           temp++; 
+        }
+        for(int index = 1; index<array_char.length;index++)
+        {
+            if((array_char[index - 1] ==' ')&(array_char[index] == simvol_array[0]))
+            {
+                temp++;
+            }
+        }
+        System.out.println(temp +" Коливчесво одинаковых заглавных букв");
     }
 }
